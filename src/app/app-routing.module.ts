5da59ead03e0fd3cea19cb2eebbcb99d8339efd3
Codruta20 <codruta_bucuroiu@yahoo.com>
2023-04-07 14:58:46 +0300

    Created routing for create plan button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { BMICalculatorComponent } from './bmi-calculator/bmi-calculator.component';
 import { StartComponent } from './start/start.component';
 import { BMIResultComponent } from './bmi-result/bmi-result.component';
+import { CreatePlanComponent } from './create-plan/create-plan.component';
 
 const routes: Routes = [
   { path: '', component: StartComponent },
   { path: 'BMI-calculator', component: BMICalculatorComponent },
   { path: 'BMI-result', component: BMIResultComponent },
+  { path: 'create-plan', component: CreatePlanComponent },
 ];
 
 @NgModule({
